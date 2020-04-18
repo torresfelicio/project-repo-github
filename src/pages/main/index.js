a418bc017008ddab169e5caafdd6acb97a8bb5a6
torresfelicio <lucastorresfellicio@gmail.com>
2020-04-18 01:03:01 -0300

    estilizado pagina

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './styles.css';
 
 export default class Main extends Component {
 state = {
@@ -13,18 +14,26 @@ state = {
   loadRepositories = async () => {
     const response = await api.get('torresfelicio');
     this.setState({repositories: response.data})
-    console.log(response);
   };
   render() {
     return (
       <>
+      <main className="repositories">
       <img src={this.state.repositories.avatar_url}></img>
-      <h2>{this.state.repositories.name}</h2>
-      <h2>Cidade: {this.state.repositories.location}</h2>
-      <h2>Repositorios: {this.state.repositories.bio}</h2>
-      <h2>Repositorios: {this.state.repositories.public_repos}</h2>
-      <h2>Seguidores: {this.state.repositories.followers}</h2>
-      <h2>Seguindo: {this.state.repositories.following}</h2>
+      <input id="pesquisa" placeholder="Usuario" type="text"></input>
+        <div id="name">
+          <h2>{this.state.repositories.name}</h2>
+        </div>
+        <div id="repos">
+          <h2>Repositorios: {this.state.repositories.bio}</h2>
+          <h2>Repositorios: {this.state.repositories.public_repos}</h2>
+        </div>
+        <div id="inf">
+          <h2>Cidade: {this.state.repositories.location}</h2>
+          <h2>Seguidores: {this.state.repositories.followers}</h2>
+          <h2>Seguindo: {this.state.repositories.following}</h2>
+        </div>
+      </main>
       </>
     );
   }
