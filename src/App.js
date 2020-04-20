5cca2d2e79ad53448b1f5beb6746dd97fd278ed6
torresfelicio <lucastorresfellicio@gmail.com>
2020-04-20 20:33:10 -0300

    finalized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Main from  './pages/main/index';
-import Header from './components/Header/index';
 
 class App extends Component {
   render() {
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,36 +2,46 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 import './styles.css';
 
+
+
+
 export default class Main extends Component {
-state = {
+  
+  state = {
   repositories: [],
 }
 
   componentDidMount() {
+    
     this.loadRepositories();
   }
 
   loadRepositories = async () => {
-    const response = await api.get('torresfelicio');
-    this.setState({repositories: response.data})
+    var inputElement = document.getElementById('pesquisa');
+    var repoText = inputElement.value;
+    const response = await api.get(`${repoText}`);
+    this.setState({repositories: response.data}); 
   };
-  render() {
+  
+  render() { 
+    
     return (
       <>
       <main className="repositories">
       <img src={this.state.repositories.avatar_url}></img>
-      <input id="pesquisa" placeholder="Usuario" type="text"></input>
+      <input name="repo" id="pesquisa" placeholder="Usuario" type="text" ></input>
+      <button id="button" onClick={this.loadRepositories}>Procurar</button>
         <div id="name">
           <h2>{this.state.repositories.name}</h2>
         </div>
         <div id="repos">
-          <h2>Repositorios: {this.state.repositories.bio}</h2>
-          <h2>Repositorios: {this.state.repositories.public_repos}</h2>
+          <h3>Biografia: </h3><p> {this.state.repositories.bio}</p>
+          <h3>Repositorios: {this.state.repositories.public_repos}</h3>
         </div>
         <div id="inf">
-          <h2>Cidade: {this.state.repositories.location}</h2>
-          <h2>Seguidores: {this.state.repositories.followers}</h2>
-          <h2>Seguindo: {this.state.repositories.following}</h2>
+          <h3>Cidade: {this.state.repositories.location}</h3>
+          <h3>Seguidores: {this.state.repositories.followers}</h3>
+          <h3>Seguindo: {this.state.repositories.following}</h3>
         </div>
       </main>
       </>
